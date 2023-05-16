96343942bbbde8fac16f29fc45f327913a43b1a1
gryde21 <122233193+gryde21@users.noreply.github.com>
2023-05-16 22:19:32 +0200

    Update Puntuaciones.java

--- Puntuaciones.java
+++ Puntuaciones.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,19 +8,14 @@ import java.util.Map;
 public class Puntuaciones {
     private static final String RUTA_PUNTUACIONES = "puntuaciones.txt";
 
-    public Puntuaciones() {
-    }
-
     public Map<String, Integer> leerPuntuaciones() {
-        Map<String, Integer> puntuaciones = new HashMap();
+        Map<String, Integer> puntuaciones = new HashMap<>();
 
         try {
-            Files.lines(Paths.get("puntuaciones.txt")).map((linea) -> {
-                return linea.split(", ");
-            }).forEach((arr) -> {
-                puntuaciones.put(arr[0], Integer.parseInt(arr[1]));
-            });
-        } catch (IOException var3) {
+            Files.lines(Paths.get(RUTA_PUNTUACIONES))
+                    .map(linea -> linea.split(", "))
+                    .forEach(arr -> puntuaciones.put(arr[0], Integer.parseInt(arr[1])));
+        } catch (IOException e) {
             System.out.println("Error al leer el archivo de puntuaciones.");
         }
 
@@ -33,25 +23,10 @@ public class Puntuaciones {
     }
 
     public void escribirPuntuacion(String nombre, int puntuacion) {
-        try {
-            FileWriter writer = new FileWriter("puntuaciones.txt", true);
-
-            try {
-                writer.write(nombre + ", " + puntuacion + "\n");
-            } catch (Throwable var7) {
-                try {
-                    writer.close();
-                } catch (Throwable var6) {
-                    var7.addSuppressed(var6);
-                }
-
-                throw var7;
-            }
-
-            writer.close();
-        } catch (IOException var8) {
+        try (FileWriter writer = new FileWriter(RUTA_PUNTUACIONES, true)) {
+            writer.write(nombre + ", " + puntuacion + "\n");
+        } catch (IOException e) {
             System.out.println("Error al escribir en el archivo de puntuaciones.");
         }
-
     }
 }
